9bc14e0df2fec2dd3766e13c99d55ad2ade2bbde
Cavin Dsouza <43354493+cavindsouza@users.noreply.github.com>
2019-11-02 16:59:34 -0600

    modified readme

--- pandas/README.md
+++ pandas/README.md
@@ -1,1 +1,6 @@
+##### fast_pandas.py
+Try and not use `lambda` apply functions unless the function itself is complex.
+Use `swifter` to compute on dataframes in parallel.
 
+##### numeric_downcast.py
+Downcast floating point or integer columns to reduce the space occupied by a dataframe for a tradeoff in precision.
--- pandas/numeric_downcast.py
+++ pandas/numeric_downcast.py
@@ -32,3 +32,64 @@ print(B.head())
 
 mse = ((A.values - B.values)**2).mean(axis=None)
 print("MSE: {}".format(mse))
+
+"""
+OUTPUT:
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 100000 entries, 0 to 99999
+Data columns (total 1 columns):
+number    100000 non-null float64
+dtypes: float64(1)
+memory usage: 781.3 KB
+None
+      number
+0 -85.280828
+1 -10.617595
+2  96.511648
+3 -20.708218
+4 -57.507265
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 100000 entries, 0 to 99999
+Data columns (total 1 columns):
+number    100000 non-null float32
+dtypes: float32(1)
+memory usage: 390.7 KB
+None
+      number
+0 -85.280830
+1 -10.617595
+2  96.511650
+3 -20.708218
+4 -57.507263
+MSE: 2.1574547254253548e-12
+
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 100000 entries, 0 to 99999
+Data columns (total 1 columns):
+number    100000 non-null int64
+dtypes: int64(1)
+memory usage: 781.3 KB
+None
+   number
+0      95
+1      53
+2      22
+3      25
+4       7
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 100000 entries, 0 to 99999
+Data columns (total 1 columns):
+number    100000 non-null int8
+dtypes: int8(1)
+memory usage: 97.7 KB
+None
+   number
+0      95
+1      53
+2      22
+3      25
+4       7
+
+Process finished with exit code 0
+
+"""
