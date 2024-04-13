483c6d0dcdee143260c75ecdbb20583d7933a62d
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2024-04-12 22:33:08 -0400

    Update comparison.py

--- iterrows vs iterruples/comparison.py
+++ iterrows vs iterruples/comparison.py
@@ -1,22 +1,20 @@
-
 import pandas as pd
 import time
 
 size = 10**6
 print("Creating dataframe...")
-df = pd.DataFrame(data={'val': range(1, size+1)})
-solution = size * (size+1) / 2
+df = pd.DataFrame(data={"val": range(1, size + 1)})
+solution = size * (size + 1) / 2
 
 
 s = time.time()
 c = 0
 for _, row in df.iterrows():
-    c += row['val']
+    c += row["val"]
 
 t = time.time()
 assert c == solution
 print(f"sum(val): {c} | iterrows took {t-s} secs")
-############################################################
 
 s = time.time()
 c = 0
@@ -27,7 +25,6 @@ t = time.time()
 assert c == solution
 print(f"sum(val): {c} | itertuples 1 took {t-s} secs")
 
-############################################################
 s = time.time()
 c = 0
 for row in df.itertuples(index=False):
@@ -37,7 +34,6 @@ t = time.time()
 assert c == solution
 print(f"sum(val): {c} | itertuples 2 took {t-s} secs")
 
-############################################################
 s = time.time()
 c = 0
 for row in df.itertuples(name=None, index=False):
@@ -46,5 +42,3 @@ for row in df.itertuples(name=None, index=False):
 t = time.time()
 assert c == solution
 print(f"sum(val): {c} | itertuples 3 took {t-s} secs")
-
-
